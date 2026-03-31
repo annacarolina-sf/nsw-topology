f501cbaaa904d051cccca083a4cb526e6354eba2
ANNACAROLINA\annac <annacarolina.franca21@gmail.com>
2026-03-31 17:07:25 -0300

    style: ajustando o tamanho do icone da nova opção de duplicar no menu flutuante

--- src/components/canvas/ContextMenu.tsx
+++ src/components/canvas/ContextMenu.tsx
@@ -63,7 +63,7 @@ export const ContextMenu: React.FC<Props> = ({ x, y, onEdit, onDelete, onDuplica
       </svg>
       Delete
     </button>
-    
+
     {/* MODIF: Adicionando a opção de duplicar node */}
     {onDuplicate && (
       <button
@@ -72,7 +72,7 @@ export const ContextMenu: React.FC<Props> = ({ x, y, onEdit, onDelete, onDuplica
         onMouseEnter={(e) => (e.currentTarget.style.background = COLORS.surfaceHover)}
         onMouseLeave={(e) => (e.currentTarget.style.background = 'transparent')}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 -960 960 960" fill="#e8eaed">
           <path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" />
         </svg>
         Duplicate
